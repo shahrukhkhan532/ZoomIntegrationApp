cf5a7add56a134bf77a0079e06150dab6d3ed52a
shahrukh15 <87074409+shahrukh15@users.noreply.github.com>
2022-01-12 14:01:04 +0500

    Changes

---
+++ ZoomIntegrationApp/Services/Files.cs
@@ -0,0 +1,26 @@
+﻿namespace ZoomIntegrationApp.Services
+{
+    public interface IFiles
+    {
+        void WriteUserDetails(string result);
+    }
+    public class Files : IFiles
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public Files(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public void WriteUserDetails(string result)
+        {
+            string directoryPath = _environment.WebRootPath + "\\sitemaps\\";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            string filepath = directoryPath + "\\usersDetails.json";
+            File.WriteAllText(filepath, result);
+        }
+    }
+}
